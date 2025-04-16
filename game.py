b7a5ea7eac4964c69005fe1bda8d29d7dd28fe37
Mathias RAGOT <ragot.mathias30@gmail.com>
2025-04-16 16:49:31 +0200

    séparation des class dans plusieur ficher

---
+++ game.py
@@ -0,0 +1,12 @@
+import pygame
+from player import player # importer notre joueur
+
+# créer une classe qui va représenter notre jeu
+class game:
+   def __init__(self):
+      # generer notre joueur
+      self.player = player()
+      
+      
+      
+      
--- main.py
+++ main.py
@@ -1,39 +1,37 @@
 
 import pygame
+from game import game # importer notre jeu
 pygame.init()
-
-# creer une preière classe qui va representer le joueur
-class player(pygame.sprite.Sprite):
-   def __init__(self):
-      super().__init__()
-      self.health = 100
-      self.max_health = 100
-      self.attack = 10
-      self.velocity = 5
-      self.image = pygame.image.load('assets/player.png')
-      self.rect = self.image.get_rect()
 
 
 # créer une fenêtre de jeux
-screen = pygame.display.set_mode((1800, 1000))
+screen = pygame.display.set_mode((1080, 720))
 pygame.display.set_caption("Jeu pour l'anglais et la NSI")
 
 background = pygame.image.load("assets/bg.jpg") # charger l'arrière-plan
 
-player = player() # charger notre player
+game = game() # charger notre jeu
 
 
 # boucle de jeu
 running = True
 
 while running:
-   screen.blit(background, (-600, 0)) # dessiner l'arrière-plan
+   screen.blit(background, (0, -200)) # dessiner l'arrière-plan
+   screen.blit(game.player.image, game.player.rect) #appeler l'image du joueur
+   
+   
    pygame.display.flip() # mettre à jour l'affichage
-   screen.blit(player.image, player.rect) #appeler l'image du joueur
    for event in pygame.event.get():
       if event.type == pygame.QUIT:
          running = False
          pygame.quit()
          print("Fermeture du jeu")
+
+
+
+
+
+
 
 
